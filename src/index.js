00819278ab2be404cae189ebc4961f095f1f3b82
Jadson Lucena <jadsonlucena@gmail.com>
2023-10-16 17:34:25 -0300

    feat: type guards and sanitize

--- src/index.js
+++ src/index.js
@@ -31,10 +31,35 @@ app.get('/*', (req, res, next) => {
 
       next()
     } else {
-      throw new Error(JSON.stringify({ status: 401, message: 'Unauthorized' }))
+      throw new Error(JSON.stringify({
+        status: 401,
+        message: 'Unauthorized'
+      }))
     }
   } catch (err) {
     console.error(res, err)
+  }
+}, async (req, res, next) => {
+  try {
+    req.query.url = new URL(req.query.url?.trim())
+
+    req.query.fields ??= []
+    req.query.ignoreDisallow ??= false
+    req.query.webhookURL ??= []
+
+    if (typeof req.query.fields === 'string') req.query.fields = req.query.fields.trim().split(',')
+    if (typeof req.query.webhookURL === 'string') req.query.webhookURL = req.query.webhookURL.trim().split(',')
+
+    req.query.fields = [].concat(req.query.fields).flat().map(field => field.trim().toLowerCase())
+    req.query.ignoreDisallow = Boolean(req.query.ignoreDisallow)
+    req.query.webhookURL = [].concat(req.query.webhookURL).flat().map(field => field.trim())
+
+    next()
+  } catch (err) {
+    throw new Error(JSON.stringify({
+      status: 400,
+      message: err.message
+    }))
   }
 }, async (req, res, next) => {
   res.json({})
